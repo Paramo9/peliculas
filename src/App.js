6faa01384e232a2b816e6f0d38a15d90d3b63c05
Paramo9 <trollol.lapr@gmail.com>
2020-04-04 01:26:42 -0600

    inicio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
+import tempo from './tempo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App()
+  {
+    return(
+      <BrowserRouter>
+        <Route exact path="/" component={tempo} />
+      </BrowserRouter>
+    )
+  }
 
-export default App;
+  export default App
---
+++ src/tempo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class tempo extends React.Component
+    {
+        render()
+            {
+                return(
+                    <div>
+                        Hola mundo
+                    </div>
+                )
+            }
+    }
+
+export default tempo
