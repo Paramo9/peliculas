48bc986302ac04f3c6ec1a519deda149c290bab4
Paramo9 <trollol.lapr@gmail.com>
2020-04-07 13:22:17 -0500

    prueba

--- src/Barra_Navegacion.js
+++ src/Barra_Navegacion.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './barra.css'
 
 class Barra_Navegacion extends React.Component
     {
@@ -6,7 +7,29 @@ class Barra_Navegacion extends React.Component
             {
                 return(
                     <div>
-                        Barra de Navegación
+                        <nav class="menu">
+  <ol>
+    <li className="menu-item"><a href="#0">Home</a></li>
+    <li className="menu-item"><a href="#0">About</a></li>
+    <li className="menu-item">
+      <a href="#0">Widgets</a>
+      <ol className="sub-menu">
+        <li className="menu-item"><a href="#0">Big Widgets</a></li>
+        <li className="menu-item"><a href="#0">Bigger Widgets</a></li>
+        <li className="menu-item"><a href="#0">Huge Widgets</a></li>
+      </ol>
+    </li>
+    <li className="menu-item">
+      <a href="#0">Kabobs</a>
+      <ol className="sub-menu">
+        <li className="menu-item"><a href="#0">Shishkabobs</a></li>
+        <li className="menu-item"><a href="#0">BBQ kabobs</a></li>
+        <li className="menu-item"><a href="#0">Summer kabobs</a></li>
+      </ol>
+    </li>
+    <li className="menu-item"><a href="#0">Contact</a></li>
+  </ol>
+</nav>
                     </div>
                 )
             }
