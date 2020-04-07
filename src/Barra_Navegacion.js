698572a0ed7860922497a9b92a76ae3e5243563d
Paramo9 <trollol.lapr@gmail.com>
2020-04-07 13:57:28 -0500

    prueba

--- src/Barra_Navegacion.js
+++ src/Barra_Navegacion.js
@@ -8,25 +8,26 @@ class Barra_Navegacion extends React.Component
                 return(
                         <nav className="menu">
                             <ol>
-                                <li className="menu-item"><a href="#0">Home</a></li>
-                                <li className="menu-item"><a href="#0">About</a></li>
-                                <li className="menu-item">
-                                <a href="#0">Widgets</a>
-                                <ol className="sub-menu">
-                                    <li className="menu-item"><a href="#0">Big Widgets</a></li>
-                                    <li className="menu-item"><a href="#0">Bigger Widgets</a></li>
-                                    <li className="menu-item"><a href="#0">Huge Widgets</a></li>
-                                </ol>
+                                <li>
+                                    Películas
+                                    <ol>
+                                        <li>Favoritas</li>
+                                    </ol>
                                 </li>
-                                <li className="menu-item">
-                                <a href="#0">Kabobs</a>
-                                <ol className="sub-menu">
-                                    <li className="menu-item"><a href="#0">Shishkabobs</a></li>
-                                    <li className="menu-item"><a href="#0">BBQ kabobs</a></li>
-                                    <li className="menu-item"><a href="#0">Summer kabobs</a></li>
-                                </ol>
+                                <li>
+                                    Series
+                                    <ol>
+                                        <li>Favoritas</li>
+                                    </ol>
                                 </li>
-                                <li className="menu-item"><a href="#0">Contact</a></li>
+                                <li>
+                                    Mi Perfil
+                                    <ol>
+                                        <li>Resumen</li>
+                                        <li>Actualizar Datos</li>
+                                        <li>Cerrar Sesión</li>
+                                    </ol>
+                                </li>
                             </ol>
                         </nav>
                 )
