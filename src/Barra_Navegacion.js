1eac8ecd99d00e3663e837fa620bca66b171fff1
Paramo9 <trollol.lapr@gmail.com>
2020-04-07 17:55:57 -0500

    prueba

--- src/Barra_Navegacion.js
+++ src/Barra_Navegacion.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import './barra.css'
+import imagenMenu from './menu.png'
 
 class Barra_Navegacion extends React.Component
     {
         render()
             {
                 return(
-                    <div>Barra de navegacion</div>
+                    <header>
+                        <input type="checkbox" id="btn-menu" />
+                        <label htmlFor="btn-menu"><img src={imagenMenu} alt="" /></label>
+                        <nav className="menu">
+                            <ul>
+                                <li>Peliculas</li>
+                                <li>Series</li>
+                                <li>Favoritas</li>
+                                <li>Mi perfil</li>
+                                <li>Actualizar Datos</li>
+                            </ul>
+                        </nav>
+                    </header>
                 )
             }
     }
