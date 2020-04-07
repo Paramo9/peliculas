7785f46f34b559c5de0689c35629f1f68c434480
Paramo9 <trollol.lapr@gmail.com>
2020-04-07 14:08:17 -0500

    prueba

--- src/Barra_Navegacion.js
+++ src/Barra_Navegacion.js
@@ -1,37 +1,11 @@
 import React from 'react'
-import './barra.css'
 
 class Barra_Navegacion extends React.Component
     {
         render()
             {
                 return(
-                    <div className="navegacion">
-                        <nav>
-                            <ol>
-                                <li>
-                                    Películas
-                                    <ol>
-                                        <li>Favoritas</li>
-                                    </ol>
-                                </li>
-                                <li>
-                                    Series
-                                    <ol>
-                                        <li>Favoritas</li>
-                                    </ol>
-                                </li>
-                                <li>
-                                    Mi Perfil
-                                    <ol>
-                                        <li>Resumen</li>
-                                        <li>Actualizar Datos</li>
-                                        <li>Cerrar Sesión</li>
-                                    </ol>
-                                </li>
-                            </ol>
-                        </nav>
-                    </div>
+                    <div>Barra de navegacion</div>
                 )
             }
     }
