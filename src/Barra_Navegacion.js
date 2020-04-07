c4f30e1b94b91c447fcd18dd879f31a75347090f
Paramo9 <trollol.lapr@gmail.com>
2020-04-07 13:47:14 -0500

    prueba

--- src/Barra_Navegacion.js
+++ src/Barra_Navegacion.js
@@ -8,23 +8,28 @@ class Barra_Navegacion extends React.Component
                 return(
                     <div>
                         <nav className="menu">
-                            <ol>
-                                <a href="#0">Películas</a>
-                                <ol className="sub-menu">
-                                    <li className="menu-item"><a href="#0">Favoritas</a></li>
-                                </ol>
-                                <a href="#0">Series</a>
-                                <ol className="sub-menu">
-                                    <li className="menu-item"><a href="#0">Favoritas</a></li>
-                                </ol>
-                                <a href="#0">Mi Perfil</a>
-                                <ol className="sub-menu">
-                                    <li className="menu-item"><a href="#0">Resumen</a></li>
-                                    <li className="menu-item"><a href="#0">Actualizar Datos</a></li>
-                                    <li className="menu-item"><a href="#0">Cerrar Sesión</a></li>
-                                </ol>
-                            </ol>
-                        </nav>
+  <ol>
+    <li className="menu-item"><a href="#0">Home</a></li>
+    <li className="menu-item"><a href="#0">About</a></li>
+    <li className="menu-item">
+      <a href="#0">Widgets</a>
+      <ol className="sub-menu">
+        <li className="menu-item"><a href="#0">Big Widgets</a></li>
+        <li className="menu-item"><a href="#0">Bigger Widgets</a></li>
+        <li className="menu-item"><a href="#0">Huge Widgets</a></li>
+      </ol>
+    </li>
+    <li className="menu-item">
+      <a href="#0">Kabobs</a>
+      <ol className="sub-menu">
+        <li className="menu-item"><a href="#0">Shishkabobs</a></li>
+        <li className="menu-item"><a href="#0">BBQ kabobs</a></li>
+        <li className="menu-item"><a href="#0">Summer kabobs</a></li>
+      </ol>
+    </li>
+    <li className="menu-item"><a href="#0">Contact</a></li>
+  </ol>
+</nav>
                     </div>
                 )
             }
