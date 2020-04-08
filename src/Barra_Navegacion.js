d52d2e0df11dbf9613e4ca83c675485fab3b0971
Paramo9 <trollol.lapr@gmail.com>
2020-04-07 23:07:02 -0500

    prueba

--- src/Barra_Navegacion.js
+++ src/Barra_Navegacion.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './barra.css'
 import imagenMenu from './menu.png'
+import { Redirect } from 'react-router-dom'
 
 class Barra_Navegacion extends React.Component
     {
         constructor(props)
             {
                 super(props)
+                this.state = {r1: false}
                 this.handleCerrarSesion = this.handleCerrarSesion.bind(this)
             }
         
@@ -14,11 +16,15 @@ class Barra_Navegacion extends React.Component
             {
                 event.preventDefault();
                 localStorage.clear()
-                localStorage.setItem("accion", "1")
+                this.setState({r1: true})
             }
 
         render()
             {
+                if(this.state.r1)
+                    {
+                        return <Redirect to={"/peliculas/login"} />
+                    }
                 return(
                     <header>
                         <input type="checkbox" id="btn-menu" />
--- src/Inicio.js
+++ src/Inicio.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import Barra_Navegacion from './Barra_Navegacion'
-import { Redirect } from 'react-router-dom'
 
 class Inicio extends React.Component
     {
         render()
             {
-                if(localStorage.getItem("accion") == "1")
-                    {
-                        return <Redirect to={"/peliculas/login"} />
-                    }
                 return(
                     <div>
                         <h1 align="center">El Club de la Película</h1>
                         <h2 align="center">La primera regla del Club de la Película es no hablar del Club de la Película</h2>
                         <Barra_Navegacion />
-                        Inicio{localStorage.getItem("accion")}
+                        Inicio
                     </div>
                 )
             }
