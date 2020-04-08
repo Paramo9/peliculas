935ce0273d20d1ee68e8c05c0bb53cb5e4e80cec
Paramo9 <trollol.lapr@gmail.com>
2020-04-08 15:01:10 -0500

    prueba

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {BrowserRouter, Route} from 'react-router-dom'
 import Login from './Login'
 import Inicio from './Inicio'
-import Login_Incorrecto from './Login_Incorrecto'
 import Registro from './Registro'
 
 function App()
@@ -12,7 +11,6 @@ function App()
         <Route exact path="/peliculas" component={Login} />
         <Route exact path="/peliculas/login" component={Login} />
         <Route exact path="/peliculas/inicio" component={Inicio} />
-        <Route exact path="/peliculas/login_incorrecto" component={Login_Incorrecto} />
         <Route exact path="/peliculas/registro" component={Registro} />
       </BrowserRouter>
     )
