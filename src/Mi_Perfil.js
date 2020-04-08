ee4d30f23a1d1ff1d217b5150aafc75caacbd79e
Paramo9 <trollol.lapr@gmail.com>
2020-04-08 17:02:34 -0500

    prueba

--- src/Mi_Perfil.js
+++ src/Mi_Perfil.js
@@ -2,11 +2,40 @@ import React from 'react'
 
 class Mi_Perfil extends React.Component
     {
+
+        constructor(props)
+            {
+                this.state = {nombre: "", password: ""}
+                this.handleChangeNombre = this.handleChangeNombre.bind(this)
+                this.handleChangePassword = this.handleChangePassword.bind(this)
+            }
+
+        handleChangeNombre(event)
+            {
+                this.setState({nombre: event.target.value})
+            }
+
+        handleChangePassword(event)
+            {
+                this.setState({password: event.target.value})
+            }
+
         render()
             {
                 return(
                     <div>
-                        <h1>{localStorage.getItem("nombre")}</h1>
+                        <h1 align="center">{localStorage.getItem("nombre")}</h1><br /><br />
+                        <h3 align="center">Nivel: {localStorage.getItem("usuario")}</h3><br />
+                        <h2 align="center">Actualizar Datos</h2><br /><br />
+                        <form>
+                            <div align="center">
+                                <h3>
+                                <input required placeholder="E-mail" type="text" value={this.state.nombre} onChange={this.handleChangeNombre} /><br /><br />
+                                <input required placeholder="Contraseña" type="password" value={this.state.password} onChange={this.handleChangePassword} /><br /><br />
+                                <input type="button" value="Actualizar" /><br /><br />
+                                </h3>
+                            </div>
+                        </form>
                     </div>
                 )
             }
