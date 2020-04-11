38d217052a263a065f3fbeec4354e8345c2220fd
Paramo9 <trollol.lapr@gmail.com>
2020-04-10 20:32:16 -0500

    prueba

--- src/Pelicula.js
+++ src/Pelicula.js
@@ -7,7 +7,7 @@ class Pelicula extends React.Component
             {
                 return(
                     <div className="pelicula">
-                        <img src={this.props.url} width="400px" height="500px"/><br />
+                        <img src={this.props.url} width="100%" height="70%"/><br />
                         {this.props.nombre}<br />
                         {this.props.fecha}
                     </div>
