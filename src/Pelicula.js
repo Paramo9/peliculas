75f814dd2e0665c06941874326d825cca10c3f24
Paramo9 <trollol.lapr@gmail.com>
2020-04-11 18:44:04 -0500

    prueba

--- src/Pelicula.js
+++ src/Pelicula.js
@@ -6,8 +6,8 @@ class Pelicula extends React.Component
         render()
             {
                 return(
-                    <div className="pelicula">
-                        <div className="poster">
+                    <div className="pelicula" align="center">
+                        <div className="poster" align="center">
                             <img src={this.props.url} />
                             <h2>{this.props.nombre} - {this.props.fecha}</h2>
                         </div>
