dc32a22a9ded108f50ad5f9fd47c77141601cba3
Paramo9 <trollol.lapr@gmail.com>
2020-04-10 23:16:22 -0500

    prueba

--- src/Peliculas.js
+++ src/Peliculas.js
@@ -9,24 +9,23 @@ class Peliculas extends React.Component
         constructor(props)
             {
                 super(props)
-                this.state = {pagina: 1}
+                this.state = {pagina: 1, peliculas: []}
             }
 
-        mostrarpeliculas = () => {
+        componentDidMount(){
+            this.setState({peliculas: this.state.peliculas.push()})
             var app = firebase.app("firestore")
             app.firestore().collection("peliculas").get().then((data) => {
-                let peliculas = []
-                peliculas.push()
                 for(let i = data.size-(20*(this.state.pagina - 1)); i>data.size-(20*(this.state.pagina - 1))-20; i--)
                     {
                         if(i>0)
                             {
+                                alert(this.state.peliculas.length)
                                 app.firestore().collection("peliculas").doc(i.toString()).get().then((data) => {
-                                    peliculas.push(<Pelicula url="1" nombre={data.get("nombre")} fecha={data.get("fecha")} />)
+                                    this.setState({peliculas: this.state.peliculas.push(<Pelicula url="1" nombre={data.get("nombre")} fecha={data.get("fecha")} />)})
                                 })
                             }
                     }
-                return peliculas
             })
         }
 
@@ -35,7 +34,7 @@ class Peliculas extends React.Component
                 return(
                     <div id="contenido">
                         <h1 align="center">Películas</h1>
-                        {this.mostrarpeliculas()}
+                        {this.state.peliculas}
                     </div>
                 )
             }
