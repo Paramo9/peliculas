6cf284d2558e0f7c6fa8411a0ca538b5152259d2
Paramo9 <trollol.lapr@gmail.com>
2020-04-10 23:05:24 -0500

    prueba

--- src/Peliculas.js
+++ src/Peliculas.js
@@ -14,13 +14,18 @@ class Peliculas extends React.Component
 
         mostrarpeliculas = () => {
             let peliculas = []
-            let numPeliculas = 0
             peliculas.push()
             var app = firebase.app("firestore")
             app.firestore().collection("peliculas").get().then((data) => {
-                alert(data.size)
+                for(let i = data.size-(20*(this.state.pagina - 1)); i>data.size-(20*(this.state.pagina - 1))-20; i--)
+                    {
+                        if(i!=0)
+                            {
+                                alert(i)
+                            }
+                    }
             })
-            for(let i = numPeliculas-(20*(this.state.pagina - 1)); i>numPeliculas-(20*(this.state.pagina - 1))-20; i--)
+            for(let i = data.size-(20*(this.state.pagina - 1)); i>data.size-(20*(this.state.pagina - 1))-20; i--)
                 {
                     app.firestore().collection("peliculas").doc(i.toString()).get().then((data) => {
                         peliculas.push(<Pelicula url="1" nombre={data.get("nombre")} fecha={data.get("fecha")} />)
