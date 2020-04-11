d0a52967d23e8d7ffd56243c102e31284879bbf6
Paramo9 <trollol.lapr@gmail.com>
2020-04-10 23:03:31 -0500

    prueba

--- src/Peliculas.js
+++ src/Peliculas.js
@@ -18,9 +18,8 @@ class Peliculas extends React.Component
             peliculas.push()
             var app = firebase.app("firestore")
             app.firestore().collection("peliculas").get().then((data) => {
-                numPeliculas = data.size
+                alert(data.size)
             })
-            alert(numPeliculas)
             for(let i = numPeliculas-(20*(this.state.pagina - 1)); i>numPeliculas-(20*(this.state.pagina - 1))-20; i--)
                 {
                     app.firestore().collection("peliculas").doc(i.toString()).get().then((data) => {
