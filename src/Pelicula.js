bbe5c61f186f4375ad450f823393906287692d18
Paramo9 <trollol.lapr@gmail.com>
2020-04-10 21:11:02 -0500

    prueba

--- src/Pelicula.js
+++ src/Pelicula.js
@@ -9,7 +9,7 @@ class Pelicula extends React.Component
                     <div className="pelicula">
                         <div className="poster">
                             <img src={this.props.url} />
-                            <h2>{this.props.nombre} - {this.props.fecha}</h2>
+                            <h3>{this.props.nombre} - {this.props.fecha}</h3>
                         </div>
                     </div>
                 )
