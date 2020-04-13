8108ea4b31fbad8d9c43e5db718230ed2af6650a
Paramo9 <trollol.lapr@gmail.com>
2020-04-13 12:52:42 -0500

    prueba

--- src/Info_Pelicula.js
+++ src/Info_Pelicula.js
@@ -7,23 +7,51 @@ class Info_Pelicula extends React.Component
         constructor(props)
             {
                 super(props)
+                this.state = {favorita: 0, mensajeBoton: "Añadir a favoritas"}
                 this.hanldeFavoritos = this.hanldeFavoritos.bind(this)
+            }
+
+        componentDidMount()
+            {
+                var app = firebase.app("firestore")
+                app.firestore().collection("usuarios").get().then((data) => {
+                    data.forEach((doc) => {
+                        var emailf = doc.get("email")
+                        if(localStorage.getItem("email") == emailf)
+                            {
+                                doc.ref.collection("peliculasFavoritas").get().then((data2) => {
+                                    data2.forEach((doc2) => {
+                                        var nombref = doc2.get("nombre")
+                                        var fechaf = doc2.get("fecha")
+                                        if(this.props.nombre==nombref && this.props.fecha==fechaf)
+                                            {
+                                                this.setState({favorita: 1, mensajeBoton: "Eliminar de favoritas"})
+                                            }
+                                    })
+                                })
+                            }
+                    })
+                })
             }
 
         hanldeFavoritos(event)
             {
                 event.preventDefault();
                 var app = firebase.app("firestore")
-                app.firestore().collection('usuarios').get().then((data) => {
-                    data.forEach((doc) => {
-                        var emailf = doc.get("email")
-                        if(localStorage.getItem("email") == emailf)
-                            {
-                                doc.ref.collection("peliculasFavoritas").add({nombre: this.props.nombre, fecha: this.props.fecha, url: this.props.url})
-                                alert("¡Esta película se ha añadido a tus películas favoritas!")
-                            }
-                    })
-                })
+                if(this.state.favorita==0)
+                    {
+                        app.firestore().collection('usuarios').get().then((data) => {
+                            data.forEach((doc) => {
+                                var emailf = doc.get("email")
+                                if(localStorage.getItem("email") == emailf)
+                                    {
+                                        doc.ref.collection("peliculasFavoritas").add({nombre: this.props.nombre, fecha: this.props.fecha, url: this.props.url})
+                                        alert("¡Esta película se ha añadido a tus películas favoritas!")
+                                        this.forceUpdate()
+                                    }
+                            })
+                        })
+                    }
             }
 
         render()
@@ -36,7 +64,7 @@ class Info_Pelicula extends React.Component
                         </div>
                         <br />
                         <div align="center">
-                            <input type="button" value="Añadir a Favoritos" onClick={this.hanldeFavoritos} />
+                            <input type="button" value={this.state.mensajeBoton} onClick={this.hanldeFavoritos} />
                         </div>
                     </div>
                 )
