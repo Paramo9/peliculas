b858bf1cfea2c88ffc2e40a707069f79f4718c0f
Paramo9 <trollol.lapr@gmail.com>
2020-04-12 22:51:37 -0500

    Updates

---
+++ precache-manifest.f3fa4dc091270293e3bef7d052934729.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e1dea15a701b7fb48fd436ae06c415",
+    "url": "/peliculas/index.html"
+  },
+  {
+    "revision": "825b8f655e71c8d8ea0a",
+    "url": "/peliculas/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3252bdf1f2427a722e8",
+    "url": "/peliculas/static/js/2.7fb35ca1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/peliculas/static/js/2.7fb35ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "825b8f655e71c8d8ea0a",
+    "url": "/peliculas/static/js/main.c30d9ecf.chunk.js"
+  },
+  {
+    "revision": "5eec618196ac2fb320d1",
+    "url": "/peliculas/static/js/runtime-main.e0bd4262.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/peliculas/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/peliculas/precache-manifest.c7b5b413e948719e71265cd1706608c2.js"
+  "/peliculas/precache-manifest.f3fa4dc091270293e3bef7d052934729.js"
 );
 
 self.addEventListener('message', (event) => {
