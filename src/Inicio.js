0f9a634faeaa96577175232d4a9ad1325457b9ba
Paramo9 <trollol.lapr@gmail.com>
2020-04-14 19:17:33 -0500

    prueba

--- src/Inicio.js
+++ src/Inicio.js
@@ -130,20 +130,28 @@ class Inicio extends React.Component
                             <div>
                                 <h1 align="center">El Club de la Película</h1>
                                 <h2 align="center">La primer regla del Club de la Película es no hablar del Club de la Película</h2>
-                                <header>
-                                    <input type="checkbox" id="btn-menu" />
-                                    <label htmlFor="btn-menu"><img src={imagenMenu} width="30px" height="30px" alt="" /></label>
-                                    <nav className="menu">
-                                        <ul>
-                                            <li onClick={this.handlePeliculas}>Películas</li>
-                                            <li onClick={this.handlePeliculasFavoritas}>Películas Favoritas</li>
-                                            <li onClick={this.handleSeries}>Series</li>
-                                            <li onClick={this.handleSeriesFavoritas}>Series Favoritas</li>
-                                            <li onClick={this.handleMiPerfil}>Mi Perfil</li>
-                                            <li onClick={this.handleCerrarSesion}>Cerrar Sesión</li>
-                                        </ul>
-                                    </nav>
-                                </header>
+                                <div id="header">
+                                    <ul className="nav">
+                                        <li>
+                                            Películas
+                                            <ul>
+                                                <li>Películas Favoritas</li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            Series
+                                            <ul>
+                                                <li>Series Favoritas</li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            Mi Perfil
+                                            <ul>
+                                                <li>Cerrar Sesión</li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
                                 <Mi_Perfil />
                             </div>
                         )
