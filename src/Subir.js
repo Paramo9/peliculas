e155076099cd1a2c245506da7cb96a9c3b493137
Paramo9 <trollol.lapr@gmail.com>
2020-04-15 21:57:08 -0500

    prueba

--- src/Subir.js
+++ src/Subir.js
@@ -7,9 +7,9 @@ class Subir extends React.Component
                 return(
                     <div>
                         <h1 align="center">Subir</h1><br /><br />
-                        <h2 align="center">Subir Películas</h2>
                         <div className="wrapper">
                             <div id="formContent">
+                                <h2 align="center">Subir Películas</h2>
                                 <input type="file" id="archivos" name="archivos" multiple /><br /><br />
                                 <input type="button" value="Subir Películas" /><br /><br />
                             </div>
