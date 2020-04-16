f10bca8515edd4c32653414502c19e3a1d631cdf
Paramo9 <trollol.lapr@gmail.com>
2020-04-15 21:35:00 -0500

    prueba

--- src/Subir.js
+++ src/Subir.js
@@ -5,7 +5,11 @@ class Subir extends React.Component
         render()
             {
                 return(
-                    <div>Subir</div>
+                    <div>
+                        <h1 align="center">Subir</h1><br /><br />
+                        <h2 align="center">Subir Películas</h2>
+                        <div align="center"><input type="file" /></div>
+                    </div>
                 )
             }
     }
